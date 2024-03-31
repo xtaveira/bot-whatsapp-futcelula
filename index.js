6ac2e27297e020e2991ada70b2961db82b616b38
Davi Taveira <devtaveira@gmail.com>
2024-03-31 17:13:05 -0300

    first test

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const { create, Client } = require('@wppconnect-team/wppconnect');
 
 async function start(client) {
-    client.onMessage(async (message) => {
-        console.log('mensagem recebida: ',message.body)
 
-        await client.sendText(messa.from, 'Olá! Está é uma resposta automática')
+    async function showList(client, message, list) {
+      await client.sendText(message.from,`Lista FUTCÉLULA:\n${list}\nPara fazer parte da lista digite "vou (nome)"`)
+}
+
+
+let listMessage = ''
+let listCounter = 0
+
+
+    client.onMessage(async (message) => {
+        console.log(`mensagem from: ${message.from}`)
+        console.log(`mensagem recebida: ${message.body}`)
+        console.log(`mensagem recebida de grupo: ${message.isGroupMsg}`)
+
+        if (message.isGroupMsg) {
+            if (message.body == '/list' && listMessage == '') {
+                await client.reply(message.from, 'Lista criada!')
+                await showList(client,message, listMessage)
+            } if (message.body.includes('vou ') && message.body.length > 5) {
+                const name = message.body.split(' ')
+                listCounter ++
+                listMessage += `${listCounter}- ${name[1]}\n`
+                showList(client,message,listMessage)
+            }
+        }
+
     })
+
 }
 
 create().then((client) => start(client))
